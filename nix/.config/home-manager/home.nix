a5b70d271dbf7ad86241d3b201f10e2628490b00
Murderlon <merlijn@soverin.net>
2023-04-14 22:11:42 +0200

    Small nix update

--- nix/.config/home-manager/home.nix
+++ nix/.config/home-manager/home.nix
@@ -1,32 +1,23 @@
 { config, lib, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "merlijn";
   home.homeDirectory = "/Users/merlijn";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "22.11"; # Please read the comment before changing.
+  home.stateVersion = "22.11";
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = [
     pkgs.curl
     pkgs.difftastic
     pkgs.exa
+    pkgs.fd
     pkgs.fzf
     pkgs.jq
     pkgs.neovim
     pkgs.nodejs
     pkgs.redis
     pkgs.ripgrep
+    pkgs.rustup
     pkgs.sqlite
     pkgs.starship
     pkgs.stow
@@ -38,34 +29,8 @@
     pkgs.wget
     pkgs.z-lua
 
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
+    (pkgs.nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; })
   ];
-
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
 
   home.sessionVariables = {
     EDITOR = "nvim";
@@ -75,7 +40,8 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
-    programs.zsh = {
+
+  programs.zsh = {
     enable = true;
     enableAutosuggestions = true;
     enableVteIntegration = true;
@@ -86,12 +52,14 @@
       ls = "exa";
       yarn = "corepack yarn";
       pnpm = "corepack pnpm";
+      j = "z";
     };
 
-    initExtraFirst = lib.concatMapStrings (x: x + "\n") [
-      "eval $(thefuck --alias)"
-    ];
-    
+    initExtra = ''
+      #TODO: eval $(thefuck --alias --enable-experimental-instant-mode)
+      bindkey -s '^p' 'tmux-sessionizer.sh\n'
+    '';
+
     historySubstringSearch = {
       enable = true;
     };
@@ -204,5 +172,32 @@
   programs.starship = {
     enable = true;
     enableZshIntegration = true;
+    settings = {
+      format = "$character";
+      command_timeout = 2000;
+      right_format = lib.concatStrings [
+        "$git_branch"
+        "$git_commit"
+        "$git_state"
+        "$git_status"
+        "$directory"
+        "$hostname"
+        "$line_break"
+        "$status"
+      ];
+      character = {
+        success_symbol = "[λ](bold green)";
+        error_symbol = "[λ](bold red)";
+      };
+      directory = {
+        truncation_length = 2;
+        style = "fg:242";
+      };
+      git_branch = {
+        format = "[$symbol$branch]($style) ";
+        symbol = " ";
+        style = "green";
+      };
+    };
   };
 }
